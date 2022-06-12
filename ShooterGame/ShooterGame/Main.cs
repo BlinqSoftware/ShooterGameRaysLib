2cbc323594b4e0eb47f87ac514162593cab0f0d6
BlinqSoftware <blinqsoftware@gmail.com>
2022-06-12 17:49:52 +0100

    Initial Commit
    Added Rayslib

---
+++ ShooterGame/ShooterGame/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace ShooterGame
+{
+    public class Main
+    {
+        public Main()
+        {
+            Setup();
+        }
+
+        private void Setup()
+        {
+
+        }
+    }
+}
